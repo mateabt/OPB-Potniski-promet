75899252d815a33ef3e64f36e646720ebb703bd6
Matea <mateabt@gmail.com>
2023-04-24 17:54:58 +0200

    .

--- prevoz.py
+++ prevoz.py
@@ -2,43 +2,67 @@
 # -*- encoding: utf-8 -*-
 
 # uvozimo bottle.py
-from bottleext import get, post, run, request, template, redirect, static_file, url
+from bottleext import *
 
 # uvozimo ustrezne podatke za povezavo
 import auth_public as auth
 
 # uvozimo psycopg2
 import psycopg2, psycopg2.extensions, psycopg2.extras
-psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) 
+# se znebimo problemov s šumniki
 
 import os
 import hashlib
+from datetime import date, timedelta
+
+skrivnost="NaJsKrIvNoStNeJsAsKrIvNoSt"
 
 # privzete nastavitve
 SERVER_PORT = os.environ.get('BOTTLE_PORT', 8080)
 RELOADER = os.environ.get('BOTTLE_RELOADER', True)
 DB_PORT = os.environ.get('POSTGRES_PORT', 5432)
 
-#za debugiranje
-#debuger(True)
-
-@get('/static/<filename:path>')
-def static(filename):
-    return static_file(filename, root='static')
+#debug(True)
 
 
-@get("/")
+######################################################################
+
+def nastaviSporocilo(sporocilo = None):
+    # global napakaSporocilo
+    staro = request.get_cookie("sporocilo", secret=skrivnost)
+    if sporocilo is None:
+        response.delete_cookie('sporocilo') #če funkciji ne podamo ničesar, izbriše piškotek z imenom sporočilo
+    else:
+        response.set_cookie('sporocilo', sporocilo, path="/", secret=skrivnost)
+    return staro
+
+def hashGesla(s):
+    """Vrni SHA-512 hash danega UTF-8 niza. Gesla vedno spravimo v bazo
+       kodirana s to funkcijo."""
+    h = hashlib.sha512()
+    h.update(s.encode('utf-8'))
+    return h.hexdigest()
+
+
+#_____________________________________________________________
+
+@get('/')
 def index():
-    return template('zacetna.html', osebe = cur)
+    return template('zacetna.html', napaka=None)
+
 
 
 ######################################################################
 # Glavni program
 
+######################################################################
+# Glavni program
+
 # priklopimo se na bazo
-conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password, port=DB_PORT)
+baza = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password, port=DB_PORT)
 #conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogočimo transakcije
-cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+cur = baza.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 # poženemo strežnik na podanih vratih, npr. http://localhost:8080/
 if __name__ == "__main__":
