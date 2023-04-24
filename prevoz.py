a25b2b238f3d6204dcb4170362eee6d098b9ff07
Matea <mateabt@gmail.com>
2023-04-24 18:08:58 +0200

    .

--- prevoz.py
+++ prevoz.py
@@ -23,7 +23,7 @@ SERVER_PORT = os.environ.get('BOTTLE_PORT', 8080)
 RELOADER = os.environ.get('BOTTLE_RELOADER', True)
 DB_PORT = os.environ.get('POSTGRES_PORT', 5432)
 
-#debug(True)
+debug(True)
 
 
 ######################################################################
