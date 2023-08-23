46fddbd230b1076aa95959d91ba59d8babd67f20
Matea <mateabt@gmail.com>
2023-08-23 12:23:16 +0200

    .

--- uvoz/drzava.py
+++ uvoz/drzava.py
@@ -1,12 +1,12 @@
 # uvozimo ustrezne podatke za povezavo
 import auth as auth
-import csv
+
 
 # uvozimo psycopg2
 import psycopg2, psycopg2.extensions, psycopg2.extras
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
 
-
+import csv
 
 def ustvari_tabelo():
     cur.execute("""
