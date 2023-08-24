2e75746f12b648ca19250b3d1b8d8bd78bf92816
Matea <mateabt@gmail.com>
2023-08-24 11:30:22 +0200

    .

---
+++ uvoz/vlaki.py
@@ -0,0 +1,47 @@
+# uvozimo ustrezne podatke za povezavo
+import auth as auth
+
+# uvozimo psycopg2
+import psycopg2, psycopg2.extensions, psycopg2.extras
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
+
+import csv
+
+def ustvari_tabelo():
+    cur.execute("""
+        CREATE TABLE vlaki (
+            st_vlaka INTEGER PRIMARY KEY,
+            st_prestopi NUMERIC 
+            id_mesta_zacetek NUMERIC REFERENCES mesto(id),
+            id_mesta_konec NUMERIC REFERENCES mesto(id),
+            );
+    """)
+    conn.commit()
+
+def pobrisi_tabelo():
+    cur.execute("""
+        DROP TABLE vlaki;
+    """)
+    conn.commit()
+
+def uvozi_podatke():
+    with open("podatki/vlak.csv", encoding="utf-8", errors='ignore') as f:
+        rd = csv.reader(f)
+        next(rd) # izpusti naslovno vrstico
+        for r in rd:
+            cur.execute("""
+                INSERT INTO vlaki
+                (st_vlaka,st_prestopi,id_mesta_zacetek,id_mesta_konec)
+                VALUES (%s, %s, %s,%s)
+                """, r)
+            # rid, = cur.fetchone()
+            print("Uvožen vlak z ID-jem %s" % (r[0]))
+    conn.commit()
+
+
+conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password)
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
+
+#pobrisi_tabelo()
+#ustvari_tabelo()
+uvozi_podatke()
