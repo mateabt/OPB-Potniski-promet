987a75a79b62edfb7edbc6d69df346fc073fe200
Matea <mateabt@gmail.com>
2023-08-24 17:33:29 +0200

    .

--- prevoz.py
+++ prevoz.py
@@ -235,11 +235,10 @@ def osebe():
 
 ######################################################################
 
-
 # priklopimo se na bazo
-baza = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password, port=DB_PORT)
+conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password, port=DB_PORT)
 #conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogočimo transakcije
-cur = baza.cursor(cursor_factory=psycopg2.extras.DictCursor)
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 # poženemo strežnik na podanih vratih, npr. http://localhost:8080/
 if __name__ == "__main__":
